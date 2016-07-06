93efa70c21c53fcd2af9f151d7cce2b446e59f78
evility <h35765469@gmail.com>
2016-07-06 18:49:20 +0800

    創建處理離線訊息的DTO

--- chat-server/src/main/java/io/ganguo/chat/route/server/dto/OfflineMessageDTO.java
+++ chat-server/src/main/java/io/ganguo/chat/route/server/dto/OfflineMessageDTO.java
@@ -1,7 +1,33 @@
 package io.ganguo.chat.route.server.dto;
 
+import io.ganguo.chat.core.transport.DataBuffer;
+import io.ganguo.chat.core.transport.IMSerializer;
+import io.ganguo.chat.route.biz.entity.Message;
+import io.ganguo.chat.route.biz.entity.OfflineMessage;
+
 /**
  * Created by user on 2016/7/6.
  */
-public class OfflineMessageDTO {
+public class OfflineMessageDTO implements IMSerializer {
+
+    OfflineMessage offlineMessage;
+
+   public OfflineMessageDTO(OfflineMessage offlineMessage){
+       this.offlineMessage = offlineMessage;
+   }
+
+
+    public DataBuffer encode(short version) {
+        DataBuffer buffer = new DataBuffer();
+        buffer.writeStringArray(offlineMessage.getOfflineMessageArray());
+        return buffer;
+    }
+
+
+    public void decode(DataBuffer buffer, short version) {
+        if(offlineMessage == null){
+            offlineMessage = new OfflineMessage();
+        }
+        offlineMessage.setOfflineMessageArray(buffer.readStringArray());
+    }
 }
