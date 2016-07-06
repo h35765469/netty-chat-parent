c644fd2d16031ec956c8df6fbe2442c0b0521812
evility <h35765469@gmail.com>
2016-07-06 18:50:32 +0800

    創立處理離線訊息的個別class

--- chat-biz/src/main/java/io/ganguo/chat/route/biz/entity/OfflineMessage.java
+++ chat-biz/src/main/java/io/ganguo/chat/route/biz/entity/OfflineMessage.java
@@ -3,5 +3,14 @@ package io.ganguo.chat.route.biz.entity;
 /**
  * Created by user on 2016/7/6.
  */
-public class OfflineMessage {
+public class OfflineMessage  extends  BaseEntity {
+    private String[] offlineMessageArray;
+
+    public String[] getOfflineMessageArray(){
+        return offlineMessageArray;
+    }
+
+    public void setOfflineMessageArray(String[] offlineMessageArray){
+        this.offlineMessageArray = offlineMessageArray;
+    }
 }
