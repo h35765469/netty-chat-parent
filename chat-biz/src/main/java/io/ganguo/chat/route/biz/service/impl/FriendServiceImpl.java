8c34b91dd7211e29d8d2c0f66a27b76249f7d9ee
evility <h35765469@gmail.com>
2016-07-22 17:39:11 +0800

    填加removeFriend()的方法來刪除朋友

--- chat-biz/src/main/java/io/ganguo/chat/route/biz/service/impl/FriendServiceImpl.java
+++ chat-biz/src/main/java/io/ganguo/chat/route/biz/service/impl/FriendServiceImpl.java
@@ -2,18 +2,50 @@ package io.ganguo.chat.route.biz.service.impl;
 
 import io.ganguo.chat.route.biz.entity.Friend;
 import io.ganguo.chat.route.biz.repository.FriendRepository;
+import io.ganguo.chat.route.biz.service.FriendService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by user on 2016/7/19.
  */
 @Service
-public class FriendServiceImpl {
+public class FriendServiceImpl implements FriendService {
     @Autowired
     private FriendRepository friendRepository;
 
-    public Friend findByUsername(String userName){
-        return friendRepository.findByUsername(userName);
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    //儲存朋友
+    public void saveFriend(String username , String friendUsername){
+        friendRepository.save(new Friend(username,friendUsername));
     }
+
+    //獲取好友
+    public List<Friend> getFriend(String username){
+        if(!friendRepository.findByUsername(username).isEmpty()){
+            return findByUsername(username);
+        }
+        return null;
+    }
+
+    public void removeFriend(String username , String friendUsername){
+        List<Friend>list = getFriend(username);
+        for(Friend friend : list){
+            if(friend.getFriendUserName().equals(friendUsername)) {
+                mongoTemplate.remove(friend);
+            }
+        }
+    }
+
+    public List<Friend>findByUsername(String username){
+        return  friendRepository.findByUsername(username);
+    }
+
+
+
 }
