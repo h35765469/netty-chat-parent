240c4ce43b59801cd1f5a9c27affc4bd7159458f
evility <h35765469@gmail.com>
2016-07-24 11:24:25 +0800

    新增 1.friendName 2.friendArray 3.isFavorite 4.isBlock 的變數 ，get、set方法

--- chat-biz/src/main/java/io/ganguo/chat/route/biz/entity/Friend.java
+++ chat-biz/src/main/java/io/ganguo/chat/route/biz/entity/Friend.java
@@ -6,13 +6,22 @@ import org.springframework.data.mongodb.core.mapping.Document;
  * Created by user on 2016/7/19.
  */
 @Document
-public class Friend {
+public class Friend extends  BaseEntity{
 
     private String friendUserName;
-    private String userName;
+    private String friendName = "";
+    private String username;
+    private String[] friendArray;
+    private boolean isFavorite = false;
+    private boolean isBlock = false;
 
     public Friend(){
 
+    }
+
+    public Friend(String username , String friendUserName){
+        this.username = username;
+        this.friendUserName = friendUserName;
     }
 
     public String getFriendUserName(){
@@ -23,20 +32,54 @@ public class Friend {
         this.friendUserName = friendUserName;
     }
 
+    public String getFriendName(){
+        return friendName;
+    }
+
+    public void setFriendName(String friendName){
+        this.friendName = friendName;
+    }
+
     public String getUserName(){
-        return userName;
+        return username;
     }
 
-    public void setUserName(String userName){
-        this.userName = userName;
+    public void setUserName(String username){
+        this.username = username;
     }
 
+    public String[] getFriendArray(){
+        return friendArray;
+    }
+
+    public void setFriendArray(String[] friendArray){
+        this.friendArray = friendArray;
+    }
+
+    public boolean getIsFavorite(){
+        return isFavorite;
+    }
+
+    public void setIsFavorite(boolean isFavorite){
+        this.isFavorite = isFavorite;
+    }
+
+    public boolean getIsBlock(){
+        return isBlock;
+    }
+
+    public void setIsBlock(boolean isBlock){
+        this.isBlock = isBlock;
+    }
 
     @Override
     public String toString() {
         return "Friend{" +
-                "UserName=" + userName +
-                ", FriendUserName=" + friendUserName +
+                "username=" + username +
+                ", friendusername=" + friendUserName +
+                ", friendName=" + friendName +
+                ", Favorite=" + isFavorite +
+                ", Block=" + isBlock +
                 '}';
     }
 }
