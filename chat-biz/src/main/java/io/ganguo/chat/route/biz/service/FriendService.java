2f5555c6d040b5dc69561f0cf4807415e2816244
evility <h35765469@gmail.com>
2016-07-27 11:42:32 +0800

    將favoriteFriend()改為 favoriteFriend(String username,String friendUserName,int isFavorite)

--- chat-biz/src/main/java/io/ganguo/chat/route/biz/service/FriendService.java
+++ chat-biz/src/main/java/io/ganguo/chat/route/biz/service/FriendService.java
@@ -2,9 +2,16 @@ package io.ganguo.chat.route.biz.service;
 
 import io.ganguo.chat.route.biz.entity.Friend;
 
+import java.util.List;
+
 /**
  * Created by user on 2016/7/19.
  */
 public interface FriendService {
-    Friend findByUsername(String userName);
+
+    void saveFriend(String username , String frendUsername);
+    List<Friend>getFriend(String username);
+    void removeFriend(String username , String friendUsername);
+    void favoriteFriend(String username,String friendUserName,int isFavorite);
+    List<Friend> findByUsername(String username);
 }
